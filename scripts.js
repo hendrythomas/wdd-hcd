891373b5700bb633ec16893efb80d0285895ac6f
Thomas Hendry <thomas.hendry@hva.nl>
2026-03-31 12:28:40 +0200

    clean javascript

--- scripts.js
+++ scripts.js
@@ -1,36 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const volumeElem = document.getElementById('volume');
-  volumeElem.style.setProperty('--volume', '10%');
+let trackElem;
+let transcriptElem;
+let transcriptScrollElem;
 
-  const trackElem = document.getElementById("track");
+document.addEventListener('DOMContentLoaded', () => {
+  trackElem = document.getElementById('track');
   if (trackElem === null) return;
 
-  const transcriptElem = document.getElementById('transcript');
+  transcriptElem = document.getElementById('transcript-text');
   if (transcriptElem === null) return;
+
+  transcriptScrollElem = document.getElementById('transcript');
+  if (transcriptScrollElem === null) return;
   
-  // insert transcript
-  trackElem.addEventListener('load', (e) => {
-    if (trackElem.track === null) return;
-    for (const cue of trackElem.track.cues) {
-      const p = document.createElement('p');
-      // p.innerText = `${cue.startTime}: ${cue.text}`;
-      p.innerText = cue.text;
-      p.dataset.startTime = cue.startTime;
-      transcriptElem.appendChild(p);
-    }
-  });
+  trackElem.addEventListener('load', insertTranscript);
+  trackElem.addEventListener('cuechange', highlightScrollCue);
 
-  // highlight transcript
-  trackElem.addEventListener('cuechange', (e) => {
-    if (trackElem.track === null) return;
-    const activeCues = trackElem.track.activeCues;
-    
-    for (const activeCue of activeCues) {
-      const cueElem = transcriptElem.querySelector(`[data-start-time="${activeCue.startTime}"]`);
-      if (cueElem === null) continue;
+  transcriptScrollElem.addEventListener('scroll', disableCueScroll);
 
-      cueElem.classList.add('bold');
-      cueElem.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  });
+  const volumeElem = document.getElementById('volume');
+  volumeElem.style.setProperty('--volume', '10%');
 });
+
+function insertTranscript(e) {
+  if (trackElem.track === null) return;
+
+  for (const cue of trackElem.track.cues) {
+    const p = document.createElement('p');
+    p.innerText = cue.text;
+    p.dataset.startTime = cue.startTime;
+    transcriptElem.appendChild(p);
+  }
+}
+
+function highlightScrollCue(e) {
+  // highlight transcript
+  if (trackElem.track === null) return;
+  const activeCues = trackElem.track.activeCues;
+  
+  for (const activeCue of activeCues) {
+    const cueElem = transcriptElem.querySelector(`[data-start-time="${activeCue.startTime}"]`);
+    if (cueElem === null) continue;
+
+    cueElem.classList.add('bold');
+    cueElem.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+}
+
+function disableCueScroll(e) {
+  console.log('u scrolled')
+}
